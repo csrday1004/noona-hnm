4843bf3762328f969b0d8de752cd282128f8a168
JSR <csrday1004@gmail.com>
2024-03-28 01:33:38 +0900

    choice

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -10,7 +10,7 @@ const ProductCard = ({data}) => {
   return (
     <div className='상품카드 product-card' style={{padding:'10px'}}>
       <img src={img} alt='제품이미지' style={{width:'100%'}}/>
-      {choice?<div style={{color:'gray', fontSize:'11px'}}>Conscious choice</div>:null}
+      {choice?<div className='choice'>Conscious choice</div>:<div className='choice'>­</div>}
       <div>{title}</div>
       <div>₩{price.toLocaleString()}</div>
       {isNew?<small style={{color:'red'}}>신제품</small>:null}
